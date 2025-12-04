2f2570899e3445e75449043cf97ba208751b2930
guglextech <guglex.technologies@gmail.com>
2025-12-04 07:40:34 +0000

    Update the service for forecast hub

--- src/app/core/constants/services-types.ts
+++ src/app/core/constants/services-types.ts
@@ -1,8 +1,15 @@
 export enum SERVICE_TYPES {
-  Sports = 'SPORTS'
+  Sports = 'SPORTS',
+  LoveRelationships = 'LOVE_RELATIONSHIPS',
+  Jobs = 'JOBS',
+  Finance = 'FINANCE',
+  Motivation = 'MOTIVATION',
 }
 
  export const serviceTypeOptions = [
   { key: 'sports', value: SERVICE_TYPES.Sports },
-  // { key: 'lottoguide', value: SERVICE_TYPES.LottoGuide }
+  { key: 'love_relationships', value: SERVICE_TYPES.LoveRelationships },
+  { key: 'jobs', value: SERVICE_TYPES.Jobs },
+  { key: 'finance', value: SERVICE_TYPES.Finance },
+  { key: 'motivation', value: SERVICE_TYPES.Motivation }
 ];
