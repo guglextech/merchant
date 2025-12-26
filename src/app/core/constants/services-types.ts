b6a5062c054e94acd9f959cfc0f2b5fdbf0db97d
guglextech <guglex.technologies@gmail.com>
2025-12-26 07:55:18 +0000

    [WIP] +> Change wording

--- src/app/core/constants/services-types.ts
+++ src/app/core/constants/services-types.ts
@@ -4,12 +4,28 @@ export enum SERVICE_TYPES {
   Jobs = 'JOBS',
   Finance = 'FINANCE',
   Motivation = 'MOTIVATION',
+  Scholarships = 'SCHOLARSHIPS',
+  BetLord = 'BET_LORD',
+  Flashscore = 'FLASHSCORE',
+  TopMatches = 'TOP_MATCHES',
+  HuskyOdds = 'HUSKY_ODDS',
+  Sportaga = 'SPORTAGA',
+  OddsOracle = 'ODDS_ORACLE',
+  KingSolomon = 'KING_SOLOMON'
 }
 
- export const serviceTypeOptions = [
+export const serviceTypeOptions = [
   { key: 'sports', value: SERVICE_TYPES.Sports },
   { key: 'love_relationships', value: SERVICE_TYPES.LoveRelationships },
   { key: 'jobs', value: SERVICE_TYPES.Jobs },
   { key: 'finance', value: SERVICE_TYPES.Finance },
-  { key: 'motivation', value: SERVICE_TYPES.Motivation }
+  { key: 'motivation', value: SERVICE_TYPES.Motivation },
+  { key: 'scholarships', value: SERVICE_TYPES.Scholarships },
+  { key: 'bet_lord', value: SERVICE_TYPES.BetLord },
+  { key: 'flashscore', value: SERVICE_TYPES.Flashscore },
+  { key: 'top_matches', value: SERVICE_TYPES.TopMatches },
+  { key: 'husky_odds', value: SERVICE_TYPES.HuskyOdds },
+  { key: 'sportaga', value: SERVICE_TYPES.Sportaga },
+  { key: 'odds_oracle', value: SERVICE_TYPES.OddsOracle },
+  { key: 'king_solomon', value: SERVICE_TYPES.KingSolomon },
 ];
